a756b1f3a2818213054185ce1e57aa173b53e060
student <ballydhillon272@gmail.com>
2020-08-08 16:14:39 -0400

    bhalwinder1992852 5th commit

--- app/src/main/java/com/example/eshopnew/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/eshopnew/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.eshopnew.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
@@ -14,10 +16,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.eshopnew.LoginActivity;
+import com.example.eshopnew.MainActivity;
 import com.example.eshopnew.R;
 
 public class HomeFragment extends Fragment {
     ViewFlipper viewFlipper;
+    public Button btn;
 
     private HomeViewModel homeViewModel;
 
@@ -27,7 +32,14 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-
+        btn=root.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
     }
   }
