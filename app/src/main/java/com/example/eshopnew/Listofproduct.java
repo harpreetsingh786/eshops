788dfbcfca2394dee3367b366bae61f4a92064ea
Gaurav Bhardwaj <gauravs10sharma@gmail.com>
2020-08-09 00:18:39 -0400

    Ranjit Singh Commit

---
+++ app/src/main/java/com/example/eshopnew/Listofproduct.java
@@ -0,0 +1,14 @@
+package com.example.eshopnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listofproduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listofproduct);
+    }
+}
